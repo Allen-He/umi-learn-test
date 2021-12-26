fd6d46abf77ced70f9eb52e3a98cfca1ad5b55e2
Allen-He <2426567439@qq.com>
2021-12-26 21:43:42 +0800

    feat: 使用“umijs”开发学生管理系统（测试学生详情页）

--- src/pages/students/[id].js
+++ src/pages/students/[id].js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function $id() {
+export default function detail(props) {
   return (
     <div>
-      学生详情页
+      <p>学生详情页</p>
+      <br />
+      <p>学号：{props.match.params.id}</p>
     </div>
   )
 }
